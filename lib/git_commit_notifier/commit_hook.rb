389c51db46920de5c6436a1888b4f1ec5f6c397f
James Berry <james@jberry.us>
2013-05-01 15:48:29 -0700

    Fudge email send date to ensure ordering of commit messages

--- lib/git_commit_notifier/commit_hook.rb
+++ lib/git_commit_notifier/commit_hook.rb
@@ -95,6 +95,11 @@ module GitCommitNotifier
       # @return [NilClass] nil
       # @see config
       def run(config_name, rev1, rev2, ref_name)
+      
+        if @start_time.nil?
+          @start_time = Time.new
+          @start_time_offset = 0
+        end
 
         # Load the configuration
         if File.exists?(config_name) 
@@ -211,7 +216,9 @@ module GitCommitNotifier
             :from_alias => result[:commit_info][:author],
             :reply_to_address => config["reply_to_author"] ? result[:commit_info][:email] : config["from"] || result[:commit_info][:email],
             :subject => subject,
-            :date => result[:commit_info][:date],
+            :commit_date => result[:commit_info][:date],
+            :current_date => Time.new.rfc2822,
+            :offset_date => @start_time + @start_time_offset.rfc2822,
             :text_message => text.join("------------------------------------------\n\n"),
             :html_message => html.join("<hr /><br />"),
             :old_rev => rev1,
@@ -235,6 +242,8 @@ module GitCommitNotifier
               :repo_name => repo_name
             )
             webhook.send
+            
+            @start_time_offset += 1
           end
         else
           commit_number = 1
@@ -258,7 +267,9 @@ module GitCommitNotifier
               :from_alias => result[:commit_info][:author],
               :reply_to_address => config["reply_to_author"] ? result[:commit_info][:email] : config["from"] || result[:commit_info][:email],
               :subject => subject,
-              :date => result[:commit_info][:date],
+              :commit_date => result[:commit_info][:date],
+              :current_date => Time.new.rfc2822,
+              :offset_date => @start_time + @start_time_offset.rfc2822,
               :text_message => result[:text_content],
               :html_message => result[:html_content],
               :old_rev => rev1,
@@ -285,6 +296,8 @@ module GitCommitNotifier
             end
 
             commit_number += 1
+            
+            @start_time_offset += 1
           end
         end
         nil
